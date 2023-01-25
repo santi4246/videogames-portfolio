ef74f4f0464d7de8caa19c08e7f954e4a1943a00
Santiago L. Romero <santiago_pna@hotmail.com>
2023-01-25 14:08:25 -0300

    Initial files v1.0.0

--- api/src/app.js
+++ api/src/app.js
@@ -21,11 +21,11 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
-
-server.use('/api', routes);
 server.get('/', (req, res) => {
   res.status(200).json({message: `App's videogames successfully online`});
 });
+
+server.use('/api', routes);
 
 // Error catching endware.
 server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
